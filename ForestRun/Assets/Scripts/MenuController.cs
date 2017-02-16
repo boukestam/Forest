0944b121790077ba74536da4cf9009bfad26faf6
timonvandenbrink <timon.van.den.brink@gmail.com>
2017-02-16 14:11:25 +0100

    commit to merge

---
+++ ForestRun/Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    void Start()
+    {
+        Button b = gameObject.GetComponent<Button>();
+        b.onClick.AddListener(delegate () { StartGame("Main"); });
+    }
+
+    public void StartGame(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
