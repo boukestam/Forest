7a45ff389377f0373b582cb3239ce27a3eb2ccbe
timonvandenbrink <timon.van.den.brink@gmail.com>
2017-03-09 09:53:17 +0100

    Added level selection functionality

---
+++ ForestRun/Assets/Scripts/DynamicSaveSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicSaveSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //TODO SAVE LEVEL UNLOCKED
+        //PlayerPrefs.SetInt("Level" + levelNumber, 1);
+        //PlayerPrefs.SetInt("Level" + levelNumber + "_score", score);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- ForestRun/Assets/Scripts/MenuController.cs
+++ ForestRun/Assets/Scripts/MenuController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour {
     
+    public enum MenuStates { Main, Levels };
+    public MenuStates currentState;
+
+
     public float ForwardSpeed = 1;
     public float JumpForce = 1;
     public float RunAnimationSpeed = 3f;
@@ -18,7 +20,16 @@ public class MenuController : MonoBehaviour {
     private GameObject Player;
     private GameObject Dog;
 
-    public void StartGame(string sceneName)
+    private GameObject mainMenu;
+    private GameObject levelMenu;
+
+    void Awake()
+    {
+        currentState = MenuStates.Main;
+        
+    }
+
+    public void OnStartGame(string sceneName)
     {
         if (!loadLock)
         {
@@ -32,7 +43,21 @@ public class MenuController : MonoBehaviour {
             loadLock = true;
         }
     }
-    
+
+    public void OnSelectLevel()
+    {
+        currentState = MenuStates.Levels;
+    }
+
+    public void OnMainMenu()
+    {
+        currentState = MenuStates.Main;
+    }
+
+    public void OnExitGame()
+    {
+        Application.Quit();
+    }
 
     void Start()
     {
@@ -40,14 +65,32 @@ public class MenuController : MonoBehaviour {
 
         Dog = GameObject.Find("Dog");
         Dog.GetComponent<Animation>()["Running"].speed = RunAnimationSpeed;
+
+        mainMenu = GameObject.FindWithTag("MainMenu");
+        levelMenu = GameObject.FindWithTag("LevelMenu");
     }
-    
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case MenuStates.Main:
+                mainMenu.SetActive(true);
+                levelMenu.SetActive(false);
+                break;
+            case MenuStates.Levels:
+                mainMenu.SetActive(false);
+                levelMenu.SetActive(true);
+                break;
+        }        
+    }
+
     void FixedUpdate()
     {
         Player.transform.position = new Vector3(
             Player.transform.position.x,
             Player.transform.position.y,
-            Player.transform.position.z + (10.0f * Time.deltaTime)
+            Player.transform.position.z + (10.0f * Time.fixedDeltaTime)
         );
     }
 }
