15bc865b4156554d81ad2012d1f03fed7559bff2
Timon van den Brink <Timon van den Brink>
2017-03-21 13:21:26 +0100

    added Android and iOS support

---
+++ ForestRun/Assets/Scripts/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+    public float RunAnimationSpeed = 3f;
+
+    private GameObject Dog;
+
+    void Start() {
+        Dog = GameObject.Find("Dog");
+        Dog.GetComponent<Animation>()["Running"].speed = RunAnimationSpeed;
+    }
+
+    public void OnMainMenu() {
+        LoadScene("Menu");
+    }
+
+    public void OnExitGame() {
+        Application.Quit();
+    }
+
+    void LoadScene(string sceneName) {
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        Scene nextScene = SceneManager.GetSceneByName(sceneName);
+        if (nextScene.IsValid()) {
+            SceneManager.LoadScene(nextScene.buildIndex);
+        }
+    }
+}
